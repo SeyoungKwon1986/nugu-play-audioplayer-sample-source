75e93928690eda3c987d1b3f8621bb2b9b44c3f3
권세영님/사용자품질 Cell <1111795@sktelecom.com>
2019-07-22 17:54:11 +0900

    port change 3000 -> 4000

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  SERVER_PORT: process.env.SERVER_PORT || 3000,
+  SERVER_PORT: process.env.SERVER_PORT || 4000,
   DOMAIN: 'http://static.naver.net/clova/service/native_extensions/sound_serise'
 }
